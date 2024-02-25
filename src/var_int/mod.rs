8c2f0e83d5bf26bc7aac94e25cb45c352192da9a
Nik Deapen <nik@deapen.com>
2024-02-25 05:47:27 -0800

    [var-int] Added support for var-int-size values. (#8)

--- src/var_int/mod.rs
+++ src/var_int/mod.rs
@@ -2,8 +2,10 @@ pub use var_int_128::*;
 pub use var_int_16::*;
 pub use var_int_32::*;
 pub use var_int_64::*;
+pub use var_int_size::*;
 
 mod var_int_128;
 mod var_int_16;
 mod var_int_32;
 mod var_int_64;
+mod var_int_size;
