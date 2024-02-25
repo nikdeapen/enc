75342145f54a145c342ee5de2e5c5986acea3d38
Nik Deapen <nik@deapen.com>
2024-02-25 05:58:27 -0800

    [hex] Added support for hexadecimal validation. (#11)

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,5 +1,7 @@
 pub use decoder::*;
 pub use encoder::*;
+pub use validator::*;
 
 mod decoder;
 mod encoder;
+mod validator;
--- src/hex/mod.rs
+++ src/hex/mod.rs
@@ -1,5 +1,7 @@
 pub use hex_decoder::*;
 pub use hex_encoder::*;
+pub use hex_validator::*;
 
 mod hex_decoder;
 mod hex_encoder;
+mod hex_validator;
