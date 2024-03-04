87321136338800cef9ef0f294e928ba68fa765fa
Nik Deapen <nik@deapen.com>
2024-03-04 12:39:42 -0800

    [*] Fixed a read-limit bug. (#22)

--- src/read/read_limit.rs
+++ src/read/read_limit.rs
@@ -14,6 +14,32 @@ impl<'a, R: Read> Read for ReadLimit<'a, R> {
         } else {
             buf
         };
-        self.read.read(buf)
+        let read: usize = self.read.read(buf)?;
+        self.limit -= read;
+        Ok(read)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+    use std::io::{Cursor, Read};
+
+    use crate::ReadLimit;
+
+    #[test]
+    fn read() -> Result<(), io::Error> {
+        let mut cursor: Cursor<Vec<u8>> = Cursor::new(vec![0, 1, 2, 3, 4]);
+        let mut limit: ReadLimit<Cursor<Vec<u8>>> = ReadLimit {
+            read: &mut cursor,
+            limit: 4,
+        };
+
+        let mut buffer: Vec<u8> = Vec::default();
+        let read: usize = limit.read_to_end(&mut buffer)?;
+        assert_eq!(read, 4);
+        assert_eq!(buffer, vec![0, 1, 2, 3]);
+
+        Ok(())
     }
 }
