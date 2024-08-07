e44066af6caeaa5eb6843374b0920a0437a43a68
Nik Deapen <nik@deapen.com>
2024-08-06 17:21:54 -0700

    [base-64] Added support for base-64 encoded data.

---
+++ src/data/base_64/decode/decode_block_last_2.rs
@@ -0,0 +1,48 @@
+/// Decodes the last block of 2 bytes without padding.
+/// - Decoding 2 chars produces 12 bits which is compressed to 1 byte.
+/// - The last 4 bits are invalid and discarded.
+/// - Returns the number of decoded bytes. (1)
+#[inline(always)]
+pub unsafe fn decode_block_last_2(
+    decoding_table: &[u8; 256],
+    data: &[u8],
+    target: &mut [u8],
+) -> usize {
+    debug_assert_eq!(data.len(), 2);
+    debug_assert_eq!(target.len(), 1);
+
+    let a: u32 = *decoding_table.get_unchecked(data[0] as usize) as u32;
+    let b: u32 = *decoding_table.get_unchecked(data[1] as usize) as u32;
+
+    let bits: u32 = (a << 2) | (b >> 4);
+
+    *target.get_unchecked_mut(0) = bits as u8;
+
+    1
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::base_64::decode::decode_block_last_2::decode_block_last_2;
+    use crate::base_64::decode::decoding_table::DecodingTable;
+
+    #[test]
+    fn fn_decode_block() {
+        let test_cases: &[(&str, &[u8])] = &[
+            ("AA", b"\x00"),
+            ("ZZ", b"\x65"),
+            ("aa", b"\x69"),
+            ("zz", b"\xCF"),
+            ("//", b"\xFF"),
+        ];
+        let decoding_table: DecodingTable = DecodingTable::default();
+        let decoding_table: &[u8; 256] = decoding_table.decoding_table();
+        for (data, expected) in test_cases {
+            let mut target: [u8; 1] = [0u8; 1];
+            let result: usize =
+                unsafe { decode_block_last_2(decoding_table, data.as_bytes(), &mut target) };
+            assert_eq!(result, 1);
+            assert_eq!(&target[..1], *expected, "data={}", *data);
+        }
+    }
+}
---
+++ src/data/base_64/decode/decoded_len_last_block.rs
@@ -0,0 +1,48 @@
+use crate::base_64::decode::remove_padding_last_block::remove_padding_last_block;
+
+/// Gets the decoded length of the last block.
+/// - The padding, if given, will be ignored. See `remove_padding_last_block`.
+#[inline(always)]
+pub unsafe fn decoded_length_last_block(data: &[u8], padding: Option<u8>) -> usize {
+    debug_assert!(data.len() <= 4);
+
+    let data: &[u8] = remove_padding_last_block(data, padding);
+    match data.len() {
+        0 => 0, // the data is empty
+        1 => 1, // invalid, we assume two more 0 bits
+        2 => 1, // may be invalid, we ignore the last 4 bits
+        3 => 2, // may be invalid, we ignore the last 2 bits
+        4 => 3, // a full block of data
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::base_64::decode::decoded_len_last_block::decoded_length_last_block;
+
+    #[test]
+    fn fn_decoded_len_last_block() {
+        let test_cases: &[(Option<u8>, &str, usize)] = &[
+            (None, "", 0),
+            (None, "A", 1),
+            (None, "AA", 1),
+            (None, "AAA", 2),
+            (None, "AAAA", 3),
+            (None, "====", 3),
+            (Some(b'='), "====", 1),
+            (None, "A===", 3),
+            (Some(b'='), "A===", 1),
+            (None, "AA==", 3),
+            (Some(b'='), "AA==", 1),
+            (None, "AAA=", 3),
+            (Some(b'='), "AAA=", 2),
+            (None, "AAAA", 3),
+            (Some(b'='), "AAAA", 3),
+        ];
+        for (padding, data, expected) in test_cases {
+            let result: usize = unsafe { decoded_length_last_block(data.as_bytes(), *padding) };
+            assert_eq!(result, *expected, "pad={:?} data={}", *padding, *data);
+        }
+    }
+}
