a5fbaa588c307b0331ba17232411742f92aa368f
Nik Deapen <nik@deapen.com>
2024-09-17 11:25:56 -0700

    [base-64] Added support for base-64 encoded data.

---
+++ src/data/base_64/decode/decode_block_last_1.rs
@@ -0,0 +1,48 @@
+/// Decodes the last block of 1 byte without padding.
+///
+/// Decoding 1 char produces 6 bits which is expanded to 1 byte.
+/// The last 2 bits are inserted as 0.
+/// Returns the number of decoded bytes. (1)
+#[inline(always)]
+pub unsafe fn decode_block_last_1(
+    decoding_table: &[u8; 256],
+    data: &[u8],
+    target: &mut [u8],
+) -> usize {
+    debug_assert_eq!(data.len(), 1);
+    debug_assert_eq!(target.len(), 1);
+
+    let a: u32 = *decoding_table.get_unchecked(data[0] as usize) as u32;
+
+    let bits: u32 = a << 2;
+
+    *target.get_unchecked_mut(0) = bits as u8;
+
+    1
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::base_64::decode::decode_block_last_1::decode_block_last_1;
+    use crate::base_64::decode::decoding_table::DecodingTable;
+
+    #[test]
+    fn fn_decode_block() {
+        let test_cases: &[(&str, &[u8])] = &[
+            ("A", b"\x00"),
+            ("Z", b"\x64"),
+            ("a", b"\x68"),
+            ("z", b"\xCC"),
+            ("/", b"\xFC"),
+        ];
+        let decoding_table: DecodingTable = DecodingTable::default();
+        let decoding_table: &[u8; 256] = decoding_table.decoding_table();
+        for (data, expected) in test_cases {
+            let mut target: [u8; 1] = [0u8; 1];
+            let result: usize =
+                unsafe { decode_block_last_1(decoding_table, data.as_bytes(), &mut target) };
+            assert_eq!(result, 1);
+            assert_eq!(&target[..1], *expected, "data={}", *data);
+        }
+    }
+}
