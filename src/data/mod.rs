d7c5b8ac3e88a679aaac6e5a25ac6448d23c2848
Nik Deapen <nik@deapen.com>
2025-01-12 16:12:51 -0800

    [data] Added support for encoding data.

---
+++ src/data/mod.rs
@@ -0,0 +1,9 @@
+pub use decoder::*;
+pub use encoder::*;
+pub use string_encoder::*;
+pub use validator::*;
+
+mod decoder;
+mod encoder;
+mod string_encoder;
+mod validator;
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+#[cfg(feature = "data")]
+pub use data::*;
 pub use error::*;
 pub use util::*;
 #[cfg(feature = "value")]
 pub use value::*;
 
+#[cfg(feature = "data")]
+mod data;
 mod error;
 mod util;
 #[cfg(feature = "value")]
