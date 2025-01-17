37b0503ef7e477f86b97d2391f2a2d79a987eedc
Nik Deapen <nik@deapen.com>
2025-01-16 16:45:42 -0800

    [value] Fixed a bug in reading an exact number of bytes.

--- src/util/exact_read.rs
+++ src/util/exact_read.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::io::ErrorKind::InvalidData;
 use std::io::{Error, Read};
 
@@ -21,10 +22,11 @@ impl<'a, R: Read> ExactRead<'a, R> {
 
 impl<'a, R: Read> Read for ExactRead<'a, R> {
     fn read(&mut self, buffer: &mut [u8]) -> std::io::Result<usize> {
-        if self.remaining == 0 {
+        if self.remaining == 0 || buffer.len() == 0 {
             Ok(0)
         } else {
-            let buffer: &mut [u8] = &mut buffer[..self.remaining];
+            let max_to_read: usize = min(buffer.len(), self.remaining);
+            let buffer: &mut [u8] = &mut buffer[..max_to_read];
             match self.read.read(buffer) {
                 Ok(read) => {
                     if read == 0 {
