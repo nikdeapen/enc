692503941005870b36e532443d3a4117fbaa9ca9
Nik Deapen <nik@deapen.com>
2025-02-10 09:38:29 -0800

    [crap

--- src/value/decode_from_read.rs
+++ src/value/decode_from_read.rs
@@ -10,7 +10,7 @@ pub trait DecodeFromRead: Sized {
     where
         R: Read;
 
-    /// Decodes a length-prefixed for from the `Read` prefix.
+    /// Decodes a length-prefixed value from the `Read` prefix.
     #[cfg(feature = "var-int")]
     fn decode_from_read_length_prefixed<R>(r: &mut R) -> Result<Self, Error>
     where
@@ -19,7 +19,7 @@ pub trait DecodeFromRead: Sized {
         Self::decode_from_read_length_prefixed_with_first_byte(crate::read_single_byte(r)?, r)
     }
 
-    /// Decodes a length-prefixed for from the `Read` prefix given the first byte.
+    /// Decodes a length-prefixed value from the `Read` prefix given the first byte.
     #[cfg(feature = "var-int")]
     fn decode_from_read_length_prefixed_with_first_byte<R>(
         first: u8,
@@ -31,7 +31,13 @@ pub trait DecodeFromRead: Sized {
         use crate::DecodeFromReadPrefix;
         let len_prefix: usize =
             crate::var_int::VarIntSize::decode_from_read_prefix_with_first_byte(first, r)?.value;
+
+        // TODO -- Boxing
+        // Boxing is used here because recursive decoders cause compilation errors when creating
+        // an infinitely recursive `ExactRead<ExactRead<ExactRead..`. I currently know no way to do
+        // this that keeps the same simplicity as just having a bit of allocation.
         let mut read: Box<dyn Read> = Box::new(crate::ReadExact::new(r, len_prefix));
+
         Self::decode_from_read(&mut read)
     }
 }
