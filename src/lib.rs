63037290be475c4a3e17176683f608d498d15c42
Nik Deapen <nik@deapen.com>
2025-02-09 18:06:28 -0800

    [value] Added support for value encoding.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@
 pub use data::*;
 pub use error::*;
 pub use util::*;
+#[cfg(feature = "value")]
+pub use value::*;
 
 #[cfg(feature = "data")]
 mod data;
 mod error;
 mod util;
+#[cfg(feature = "value")]
+mod value;
---
+++ src/value/decode_from_read.rs
@@ -0,0 +1,12 @@
+use std::io::{Error, Read};
+
+/// A value that can decode itself from a `Read`.
+///
+/// # Note
+/// The decoder must fully drain the `Read`.
+pub trait DecodeFromRead: Sized {
+    /// Decodes a value from the `Read`.
+    fn decode_from_read<R>(r: &mut R) -> Result<Self, Error>
+    where
+        R: Read;
+}
