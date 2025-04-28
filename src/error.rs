0f8998c0b770cdee78e8ac847d6f315ea4041c15
Nik Deapen <nik@deapen.com>
2025-04-27 19:05:09 -0700

    [core] Added support for encoding errors.

---
+++ src/error.rs
@@ -0,0 +1,47 @@
+use std::fmt::{Display, Formatter};
+use std::io;
+use std::io::ErrorKind;
+
+/// An error processing encoded data.
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum Error {
+    /// An integer overflowed.
+    IntegerOverflow,
+
+    /// The target buffer had insufficient space.
+    InsufficientTargetSpace,
+
+    /// The encoded data was invalid.
+    InvalidEncodedData,
+}
+
+impl Error {
+    //! Display
+
+    /// Gets the error message.
+    pub const fn message(&self) -> &'static str {
+        match self {
+            Self::IntegerOverflow => "integer overflow",
+            Self::InsufficientTargetSpace => "insufficient target space",
+            Self::InvalidEncodedData => "invalid encoded data",
+        }
+    }
+}
+
+impl From<Error> for io::Error {
+    fn from(error: Error) -> Self {
+        match error {
+            Error::IntegerOverflow => io::Error::new(ErrorKind::OutOfMemory, error),
+            Error::InsufficientTargetSpace => io::Error::new(ErrorKind::InvalidInput, error),
+            Error::InvalidEncodedData => io::Error::new(ErrorKind::InvalidData, error),
+        }
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl std::error::Error for Error {}
