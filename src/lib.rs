f7b9bd8e1d446015d4affff25eb7262e6c8d4793
Nik Deapen <nik@deapen.com>
2025-07-06 17:28:56 -0700

    [value] Added support for value encoding.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 #[cfg(feature = "data")]
 pub use data::*;
 pub use error::*;
+pub use stream_error::*;
+#[cfg(feature = "value")]
+pub use value::*;
 
 #[cfg(feature = "data")]
 mod data;
 mod error;
+mod stream_error;
+#[cfg(feature = "value")]
+mod value;
