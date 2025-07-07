316aa39f3d56dd9ac993e41cb3162657193d57e9
Nik Deapen <nik@deapen.com>
2025-07-06 17:30:41 -0700

    [value] Added support for value decoding.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 pub use data::*;
 pub use error::*;
 pub use stream_error::*;
+pub use util::*;
 #[cfg(feature = "value")]
 pub use value::*;
 
@@ -9,5 +10,6 @@ pub use value::*;
 mod data;
 mod error;
 mod stream_error;
+mod util;
 #[cfg(feature = "value")]
 mod value;
---
+++ src/value/decode_from_read_prefix.rs
@@ -0,0 +1,44 @@
+use crate::{StreamError, read_optional_byte, read_single_byte};
+use std::io::Read;
+
+/// A value that can decode itself from a `Read` prefix.
+///
+/// # Note
+/// - The impl does not need to fully drain the `Read`.
+/// - The impl must read the entire encoded value.
+/// - The impl must not read past the end of the encoded value.
+/// - An encoded value must be at least one byte in length.
+pub trait DecodeFromReadPrefix: Sized {
+    /// Decodes a value from the `Read` prefix.
+    fn decode_from_read_prefix<R>(r: &mut R) -> Result<Self, StreamError>
+    where
+        R: Read,
+    {
+        let first: u8 = read_single_byte(r)?;
+        Self::decode_from_read_prefix_with_first_byte(r, first)
+    }
+
+    /// Decodes a value from the `Read` prefix given the `first` byte.
+    fn decode_from_read_prefix_with_first_byte<R>(
+        r: &mut R,
+        first: u8,
+    ) -> Result<Self, StreamError>
+    where
+        R: Read;
+
+    /// Decodes an optional value from the `Read` prefix.
+    ///
+    /// Returns `None` if the `Read` is empty.
+    fn decode_from_read_prefix_optional<R>(r: &mut R) -> Result<Option<Self>, StreamError>
+    where
+        R: Read,
+    {
+        if let Some(first) = read_optional_byte(r)? {
+            Ok(Some(Self::decode_from_read_prefix_with_first_byte(
+                r, first,
+            )?))
+        } else {
+            Ok(None)
+        }
+    }
+}
