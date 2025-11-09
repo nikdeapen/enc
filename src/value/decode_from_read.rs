b1dea9e7b7147f7bae245c283ec5260595db6199
Nik Deapen <nik@deapen.com>
2025-11-09 10:10:42 -0800

    [value] Added support for reading length-prefixed values.

--- src/value/decode_from_read.rs
+++ src/value/decode_from_read.rs
@@ -10,4 +10,30 @@ pub trait DecodeFromRead: Sized {
     fn decode_from_read<R>(r: &mut R) -> Result<Self, Error>
     where
         R: Read;
+
+    /// Decodes a length-prefixed value from the `Read` prefix.
+    #[cfg(feature = "var-int")]
+    fn decode_from_read_length_prefixed<R>(r: &mut R) -> Result<Self, Error>
+    where
+        R: Read,
+    {
+        let first: u8 = crate::read_single_byte(r)?;
+        Self::decode_from_read_length_prefixed_with_first_byte(r, first)
+    }
+
+    /// Decodes a length-prefixed value from the `Read` prefix given the `first` byte.
+    #[cfg(feature = "var-int")]
+    fn decode_from_read_length_prefixed_with_first_byte<R>(
+        r: &mut R,
+        first: u8,
+    ) -> Result<Self, Error>
+    where
+        R: Read,
+    {
+        use crate::var_int::VarIntSize;
+        use crate::DecodeFromReadPrefix;
+
+        let prefix: usize = VarIntSize::decode_from_read_prefix_with_first_byte(r, first)?.value();
+        Self::decode_from_read(&mut r.take(prefix as u64))
+    }
 }
