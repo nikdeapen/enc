33113e83dc2e6c6ccb13cc96a52bd046fa2ffb44
Nik Deapen <nik@deapen.com>
2025-11-09 10:11:38 -0800

    [value] Added the impl_decode_from_read_prefix_length_prefixed macro.

---
+++ src/value/impls/impl_decode_length_prefixed.rs
@@ -0,0 +1,19 @@
+/// Proxies the implementation of the `DecodeFromReadPrefix` trait for the `target_type` by using
+/// the `DecodeFromRead::decode_from_read_length_prefixed_with_first_byte` function.
+#[macro_export]
+macro_rules! impl_decode_from_read_prefix_length_prefixed {
+    ($target_type:ty) => {
+        impl $crate::DecodeFromReadPrefix for $target_type {
+            fn decode_from_read_prefix_with_first_byte<R>(
+                r: &mut R,
+                first: u8,
+            ) -> Result<Self, $crate::Error>
+            where
+                R: std::io::Read,
+            {
+                use $crate::DecodeFromRead;
+                Self::decode_from_read_length_prefixed_with_first_byte(r, first)
+            }
+        }
+    };
+}
