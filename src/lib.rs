aef8cb1f04271b24facb0faf1b215c04443d0014
Nik Deapen <nik@deapen.com>
2025-11-09 10:00:07 -0800

    [value] Added support for decoding values.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #[cfg(feature = "data")]
 pub use data::*;
 pub use error::*;
+pub use util::*;
 #[cfg(feature = "value")]
 pub use value::*;
 
 #[cfg(feature = "data")]
 mod data;
 mod error;
+mod util;
 #[cfg(feature = "value")]
 mod value;
--- src/value/mod.rs
+++ src/value/mod.rs
@@ -1,7 +1,11 @@
+pub use decode_from_read::*;
+pub use decode_from_read_prefix::*;
 pub use encode_to_slice::*;
 pub use encode_to_write::*;
 pub use encoded_len::*;
 
+mod decode_from_read;
+mod decode_from_read_prefix;
 mod encode_to_slice;
 mod encode_to_write;
 mod encoded_len;
