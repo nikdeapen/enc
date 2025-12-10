6ea8e15c809d46f812d04009e939e8c6230e7061
Nik Deapen <nik@deapen.com>
2025-12-10 11:53:24 -0800

    [var-int] Added macro decoding to var-int types. (#3)

--- src/value/decode_from_read.rs
+++ src/value/decode_from_read.rs
@@ -6,7 +6,8 @@ pub trait DecodeFromRead: Sized {
     /// Decodes a value from the `Read`.
     ///
     /// # Note
-    /// The implementation must fully drain the `Read`.
+    /// The `Read` should contain only the decoded value and the implementation should fully drain
+    /// the `Read` but this is not guaranteed if the value is improperly encoded.
     fn decode_from_read<R>(r: &mut R) -> Result<Self, Error>
     where
         R: Read;
